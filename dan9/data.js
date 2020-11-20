ad0d341d598414d2935bc9b1972ae68d79d25afb
= <=>
2020-11-20 10:59:28 +0100

    localStorage i JSON

---
+++ dan9/data.js
@@ -0,0 +1,42 @@
+var podaci = [
+    {
+        "naziv" : 'Lenovo Laptop 82D40030YA 15,6" /Intel Core i5/16 GB/512 GB SSD',
+        "slika" : 'https://static.tehnomanija.rs/UserFiles/products/155825.jpg',
+        "cena" : 139990
+    },
+    {
+        "naziv" : 'Samsung Smart televizor 50" UE50TU8502UXXH',
+        "slika" : 'https://static.tehnomanija.rs/UserFiles/products/151381.jpg',
+        "cena" : 69990
+    },
+    {
+        "naziv" : 'SAMSUNG Usisivač VC 4135, 1600W',
+        "slika" : 'https://static.tehnomanija.rs/UserFiles/products/10616.jpg',
+        "cena" : 7999
+    },
+    {
+        "naziv" : 'Candy Mašina za pranje i sušenje veša ROW 41494DWMCE S',
+        "slika" : 'https://static.tehnomanija.rs/UserFiles/products/156487.jpg',
+        "cena" : 74990
+    },
+    {
+        "naziv" : 'Beko Kombinovani frižider RCSA366K30XB',
+        "slika" : 'https://static.tehnomanija.rs/UserFiles/products/147531.jpg',
+        "cena" : 43999
+    },
+    {
+        "naziv" : 'Philips Kuvalo za vodu HD9305/21 - Izrađen od nerđajućeg čelika',
+        "slika" : 'https://static.tehnomanija.rs/UserFiles/products/76388.jpg',
+        "cena" : 5499
+    },
+    {
+        "naziv" : 'Tesla Električni šporet CV6400SX',
+        "slika" : 'https://static.tehnomanija.rs/UserFiles/products/143457.jpg',
+        "cena" : 39990
+    },
+    {
+        "naziv" : 'Tesy Bojler GCV 804420 B11 TSR',
+        "slika" : 'https://static.tehnomanija.rs/UserFiles/products/142360.jpg',
+        "cena" : 11990
+    }
+]
